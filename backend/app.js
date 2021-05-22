e552bf47cc1c557100606025ef2832b00282afd0
krishnakanna18 <krishnakanna562@gmail.com>
2021-05-22 22:04:04 +0530

    Db

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,15 @@ const express=require("express");
       socket=require("socket.io");
       http=require("http");
       mysql=require("mysql");
-
+      config=require("./config")
       const log = console.log
       
-// const db=mysql.createConnection({
-// host:"localhost",
-// user : 'krishna',
-// password : '0mysqldb@Krishna',
-// database: 'codeshare'
-// })
+const db=mysql.createConnection({
+host:"localhost",
+user : config.username,
+password : config.password,
+database: 'codeshare'
+})
 
 
 let query=(exp,values)=>{
@@ -53,26 +53,26 @@ io.on('connection',(socket)=>{
     socket.on('joinRoom',async(arg)=>{
 
         log('Request to join',arg.room)
-        let text=`let query=(exp,values)=>{
-            return new Promise((resolve,reject)=>{
-                db.query(exp,values,(err,results,fields)=>{
-                    if(err)
-                        reject(err);
-                    else
-                        {    
-                            resolve({results:results,fields:fields});
-                }
-                })
-            })
-            }`
-        // let res=await query("select text from room where no='"+arg.room+"';");
-        // if(res.results.length>0)
-        // {
-        //     text=res.results[0].text
-        // }
-        // else{
-        //     await query("insert into room values('"+arg.room+"','"+text+"');")
-        // }
+        // let text=`let query=(exp,values)=>{
+        //     return new Promise((resolve,reject)=>{
+        //         db.query(exp,values,(err,results,fields)=>{
+        //             if(err)
+        //                 reject(err);
+        //             else
+        //                 {    
+        //                     resolve({results:results,fields:fields});
+        //         }
+        //         })
+        //     })
+        //     }`
+        let res=await query("select text from room where no='"+arg.room+"';");
+        if(res.results.length>0)
+        {
+            text=res.results[0].text
+        }
+        else{
+            await query("insert into room values('"+arg.room+"','"+text+"');")
+        }
         socket.join(`${arg.room}`)
         socket.emit('initialiseEditor',{value:text})
         log("Joined",socket.rooms)
@@ -80,9 +80,14 @@ io.on('connection',(socket)=>{
 
     socket.on('setEditor',async(arg)=>{
         // log("Editing",socket.rooms,arg.room)
-        // await query("update room set text='"+arg.value+"' where no='"+arg.room+"';")
         socket.to(`${arg.room}`).emit('resetEditor',arg)
+        await query("update room set text='"+arg.value+"' where no='"+arg.room+"';")
     })
+
+    socket.on('shareVideo',async(arg)=>{
+        socket.to(`${arg.room}`).emit('setVideo',arg.video)
+    })
+
 
     socket.on('disconnect', () => {
       });
@@ -90,6 +95,7 @@ io.on('connection',(socket)=>{
     socket.on('closeConnection',arg=>{
         socket.leave(`${arg.room}`)
     })
+
 })
 
 server.listen(process.env.PORT || 9000,'0.0.0.0',(err)=>{
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,39 +18,38 @@ import "ace-builds/src-noconflict/theme-solarized_dark";
 import "ace-builds/src-noconflict/ext-language_tools"
 import io from 'socket.io-client'
 
-const App=()=>{
 
+
+const App=()=>{
 
   return (
     <Router>
-      <div className="container mt-5 d-flex flex-lg-row flex-column">
-          <div className="col-lg-6 col-12 mt-5">
-              <div className="d-flex flex-column container justify-content-md-center align-items-center">
-                <Link to='/room/1'>
-                  <button className="w-100 btn btn-dark m-5">
+      <div className="mt-5 d-flex flex-lg-row flex-column">
+          <div className="col-lg-2 col-12 mt-5">
+              <div className="d-flex flex-lg-column flex-row justify-content-md-center align-items-center">
+                <Link to='/room/1' className="">
+                  <button className="w-100 btn btn-dark m-5 ">
                     Room 1
                   </button>
                 </Link>
-                <Link to='/room/2'>
+                <Link to='/room/2' className="">
                   <button className="w-100 btn btn-dark m-5" >
                     Room 2
                   </button>
                 </Link>
-                <Link to='/room/3'>
+                <Link to='/room/3' className="">
                   <button className="w-100 btn btn-dark m-5" >
                     Room 3
                   </button>
                 </Link>
               </div>
           </div>
-          <div className="col-lg-6 col-12 mt-5">
-              <Switch>
-              {/* {roomNo!==0?
-              <Editor room={roomNo} socket={io(endPoint)}></Editor>
-              :""} */}
-                <Route exact path='/room/:id' component={(props)=><Editor {...props} key={props.match.id}></Editor>}></Route>
-              </Switch>
-          </div>
+          <Switch>
+          {/* {roomNo!==0?
+          <Editor room={roomNo} socket={io(endPoint)}></Editor>
+          :""} */}
+            <Route exact path='/room/:id' component={(props)=><Editor {...props} key={props.match.id}></Editor>}></Route>
+          </Switch>
       </div>
     </Router>
   )
@@ -65,11 +64,13 @@ const Editor=({location,match})=>{
   if(window.location.host==="localhost:3000") endPt="http://localhost:9000"
   else if(window.location.host==="192.168.0.13:3000") endPt="http://192.168.0.13:9000"
 
-  endPt="https://floating-tor-72233.herokuapp.com"
- console.log(endPt)
+  // endPt="https://floating-tor-72233.herokuapp.com"
+//  console.log(endPt)
   let [endPoint]=useState(endPt)
   let [socket]=useState(io(endPoint))
   let [room,setRoom]=useState(0)
+  let [isSharing,setSharing]=useState(false)
+  let [isScreenCap,setScreenCap]=useState(false)
 
 
   let theme=""
@@ -77,10 +78,50 @@ const Editor=({location,match})=>{
   else if(room===2) theme="xcode"
   else if(room===3) theme="solarized_dark"
 
+
+  let screenShare=async()=>{
+    let captureStream = null;
+    try {
+      captureStream = await navigator.mediaDevices.getDisplayMedia({video: {
+        cursor: "always"
+      },
+      audio: false});
+    } catch(err) {
+      console.error("Error: " + err);
+    }
+    return captureStream;
+  }
+
+  let displayVideo=async()=>{
+
+    let vid=await screenShare()
+    setSharing(true)
+    // socket.emit('')
+    // let videoElem=document.getElementById("videoElementSrc")
+    // videoElem.srcObject =vid
+    // vid.ondatavailable=(e)=>{
+    //   socket.emit('shareVideo',{
+    //     room:`${room}`,
+    //     video:e.data
+    //   })
+    // }
+  }
+
   useEffect(()=>{
    
     socket.on('resetEditor',val=>{
         setEditorText(val.value)
+    })
+
+    socket.on('setVideo',async(video)=>{
+
+      if(isSharing===true){
+        await stopVideo()
+      }
+      setScreenCap(true)
+      let videoElem=document.getElementById("videoElementSrc")
+      videoElem.srcObject =video
+
     })
 
     return ()=>{
@@ -107,7 +148,19 @@ const Editor=({location,match})=>{
       socket.emit('setEditor',{value:value,room:`${room}`})
   }
 
+
+  let stopVideo=async()=>{
+    let videoElem=document.getElementById("videoElementSrc")
+    let tracks = videoElem.srcObject.getTracks();
+    tracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
+    setSharing(false)
+  }
+
   return(
+    <div className="col-lg-10 col-12 mt-5 d-flex flex-lg-row flex-column">
+        <div className="col-lg-6 justify-content-center row ">
+
           <AceEditor
             name={`trialEditor:${room}`}
             theme={theme}
@@ -122,11 +175,30 @@ const Editor=({location,match})=>{
               enableLiveAutocompletion: true,
               enableSnippets: true,
             }}
+            onInput={(e)=>{console.log(e)}}
             debounceChangePeriod={500}
             width={'800px'}
             onChange={(value,e)=>{setEditor(value)} }
           >
           </AceEditor>
+
+          {isSharing===false?
+                <button className="btn btn-dark mt-5 w-50 mb-5" onClick={async()=>{await displayVideo()}}>Share your screen</button>
+                :
+                <button className="btn btn-dark mt-5 w-50 mb-5" onClick={async()=>{await stopVideo()}}>Stop sharing screen</button>
+                }
+
+  
+        </div>
+        <div className="col-lg-6 justify-content-center row" style={{maxWidth:"100%"}} >
+          <video id="videoElementSrc" autoPlay>
+
+          </video>
+        </div>
+    </div>
+
+       
+
   )
 
 
